437d24148d8da037bdb76d082480a5cfa45921ea
chuck <chucksimpson@kc.rr.com>
2017-09-02 15:12:25 -0500

    twitter

--- liri.js
+++ liri.js
@@ -1,12 +1,11 @@
 var fs = require("fs");
-var Twitter = require('twitter');
-
-
-var LoadTwitterKey = require("./keys.js");
-
+var twitter = require('twitter');
+var loadTwitterKey = require("./keys.js");
 var Spotify = require('node-spotify-api');
 // var LoadSpotifyRandom = require("./random.txt");
 
+const logFileName = "log.txt";
+var outputstring = "";
 
 // Hold the value of command "my-tweets",spotify-this-song", "movie-this", "do-what-it-says"
 var commandType = process.argv[2];
@@ -14,69 +13,60 @@ var commandType = process.argv[2];
 
 if (commandType === "my-tweets") {
   console.log("my-tweets");
- 
-  //var client = new Twitter(LoadTwitterKey);
-  
-  var client = new Twitter({
-    consumer_key: 'PLeYr4SUS9CAuBRGYCYHYyDK4',
-    consumer_secret: 'i3Jz8Vci2mdNOS2VPUTNCmBJzJx6AnUddgb2nV0yeoyjXh570G',
-    access_token_key: '903435559804305408-lABJQsWbsSEUBjnw7nl0INt2gxeOJrf',
-    access_token_secret: 'Vq3ELWHRNrPTLzhe6RaUeJPkQTUbyvnijnMFKyPqFixOv'
-  });
-  
-  
+  outputstring = commandType + '\n';
+  var client = new twitter(loadTwitterKey);
   client.get('statuses/user_timeline', function(error, tweets, response) {
     if (!error) {
-      console.log("tweets: ");
-      // console.log(tweets);
-      // console.log(this.text);
-      // console.log(response);
-      // var printTweets = tweets.text;
-         }
+      // console.log(`JS TYPE: ${typeof tweets}, ARRAY LENGTH: ${tweets.length} --------------------------------------------------------------------------------TWEET JSON!!!!`);
+      // Create an array of text strings that I'm going to get from the tweets
+      // javascript object returned by the Twitter API
+      var tweetTexts = [];      
+      // Loop through the tweets javascript object which is an array as it happens
+      for (var i = 0; i < tweets.length; i++) {
+        // console.log(`Index of Element in \"tweets\" array #${i+1} : `);
+        // Get the text property of each individual element of the tweets array 
+        //console.log( "Text of Tweets: ");
+        outputstring += '\t';
+        outputstring += tweets[i].text;
+        outputstring += '\n';
+        console.log("\t" + tweets[i].text);
+        tweets[i].text += '\n';
+        tweetTexts.push( tweets[i].text );
+      }
+      tweetTexts.push( '\n' );
+      writeLog(outputstring);
+    }
     else {
-      console.log("Error: ");
       console.log(error);
     }
   });
+  
 }
 else if (commandType === "spotify-this-song"){
   console.log("spotify-this-song");
-
 }
 else if (commandType === "movie-this") {
   console.log("movie-this");
 }
 else if (commandType === "do-what-it-says") {
-
+  console.log("do-what-it-says");
 }
 else {
-  console.log("bad command");
+  console.log("Bad Command");
+  writeLog(" Bad Command");
 }
 
 
-commandType = commandType + "\n";
 
-// Callbacks appear in familiar places, like fs.writeFile.
-fs.appendFile("log.txt", commandType, function(err) {
 
+function writeLog(stuff, file = logFileName) {
+  fs.appendFile(file, stuff, isThereAnErrorThatIUnnecessarilyNeedToLog);
+}
+
+function isThereAnErrorThatIUnnecessarilyNeedToLog(err) {
   if (err) {
-    console.log(err);
+    console.error("There was problem writing to the file!", err);
   }
-
-  console.log("File saved!");
-
-});
-
-// Alternatively, we can assign our function to a variable,
-// and pass it by name.
-// var writeFileCallback = function(err) {
-
-//   if (err) {
-//     console.log(err);
-//   }
-
-//   console.log("File saved!");
-
-// };
-
-// fs.writeFile("log.txt", "Log message!", writeFileCallback);
+  //console.log("File saved!");
+  return false; // !!!!
+}
